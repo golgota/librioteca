ae1ed4d156b39dafcadc655dc0d3a9ea1717ab79
kelvinst <kelvin.stinghen@gmail.com>
2016-04-27 10:02:55 -0300

    Updates on genesis rails done

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-gem 'genesis_rails', path: "../genesis_rails"
+gem 'genesis_rails'
 gem 'kaminari'
 gem 'responders'
 gem 'simple_form'
@@ -52,6 +52,7 @@ end
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'pry-rails'
 end
 
 group :development do
--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -2,16 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "users/index", type: :view do
   before(:each) do
-    assign(:users, [
-      FactoryGirl.create(:user),
-      FactoryGirl.create(:user)
-    ])
+    FactoryGirl.create(:user)
+    FactoryGirl.create(:user)
+    assign(:users, User.page(1))
   end
 
   it "renders a list of users" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Email".to_s, :count => 2
-    assert_select "tr>td", :text => "Password".to_s, :count => 2
+    assert_select "tr>td", :text => "kelvin".to_s, :count => 2
+    assert_select "tr>td", :text => "kelvin.s@gmail.com".to_s, :count => 2
+    assert_select "tr>td", :text => "12345678".to_s, :count => 2
   end
 end
