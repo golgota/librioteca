520e356ef961d5880244f61243e01580b4ca2354
Mingyar Furtado <mingyar.furtado@gmail.com>
2016-04-26 23:36:02 -0300

    User specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  
   def to_s
     name
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,19 @@
 FactoryGirl.define do
   factory :user do
-    name "MyString"
-    email "MyString"
-    password "MyString"
+    name "kelvin"
+    email "kelvin.s@gmail.com"
+    password "12345678"
+
+    trait :invalid do
+      name nil
+      email nil
+      password nil
+    end
+
+    trait :another do
+      name "mingyar"
+      email "mingyar.furtado@gmail.com"
+      password "87654321"
+    end
   end
 end
